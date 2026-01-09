049c7cabcb73d76d3092d940fddac338acbe6ef6
Geetesh Singh <singh.geetesh1998@gmail.com>
2026-01-09 23:56:48 +0530

    Testing github actions

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,41 @@ let server;
 // setup cors.
 
 app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200,
-  })
+    cors({
+        origin: "*",
+        credentials: true,
+        optionSuccessStatus: 200,
+    })
 );
 
 // setup body parser.
 
-app.use(body.json({ limit: "100kb" }));
+app.use(body.json({limit: "100kb"}));
 
 // load all routes.
 
 app.use("/recipes", require("./routes/recipes"));
 
 async function startServer() {
-  try {
-    // Load express app to listen on config port.
-    const port = 3000
-    server = app.listen(port, () => {
-      console.log(`Service ready on :${port}`)
-    });
-  } catch (error) {
-    console.error("Failed to connect to the database:", error);
-    process.exit(1);
-  }
+    try {
+        // Load express app to listen on config port.
+        const port = 3000
+        server = app.listen(port, () => {
+            console.log(`Service ready on :${port}`)
+        });
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        process.exit(1);
+    }
 }
 
 function stop() {
-  console.log("Stopping server");
-  server.close();
+    console.log("Stopping server");
+    server.close();
 }
 
-export { server, startServer, stop };
+export {server, startServer, stop};
 
-startServer();
+startServer().then(r =>{
+
+});
